7d085e19f4621e85a5b6a6cbc63daea616938d90
Mahender1929 <39700226+Mahender1929@users.noreply.github.com>
2023-05-25 20:55:04 +0530

    Update AES.java
    changed encription

--- src/main/java/com/cebi/utility/AES.java
+++ src/main/java/com/cebi/utility/AES.java
@@ -8,30 +8,29 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.log4j.Logger;
+import org.apache.commons.codec.binary.Base64;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+
 
 public class AES {
 	private static final String ALGORITHM = "AES";
 	private static final String KEY = "1Hbfh667adfDEJ78";
 	private static final Logger logger = Logger.getLogger(AES.class);
 
-	@SuppressWarnings("restriction")
 	public static String encrypt(String value) throws Exception {
 		Key key = generateKey();
 		Cipher cipher = Cipher.getInstance(AES.ALGORITHM);
 		cipher.init(Cipher.ENCRYPT_MODE, key);
 		byte[] encryptedByteValue = cipher.doFinal(value.getBytes("utf-8"));
-		return new BASE64Encoder().encode(encryptedByteValue);
-
+		return new Base64().encodeToString(encryptedByteValue);
 	}
 
 	public static String decrypt(String value) throws Exception {
 		Key key = generateKey();
 		Cipher cipher = Cipher.getInstance(AES.ALGORITHM);
 		cipher.init(Cipher.DECRYPT_MODE, key);
-		byte[] decryptedValue64 = new BASE64Decoder().decodeBuffer(value);
+		byte[] decryptedValue64 = new Base64().decode(value);
+		
 		byte[] decryptedByteValue = cipher.doFinal(decryptedValue64);
 		return new String(decryptedByteValue, "utf-8");
 
